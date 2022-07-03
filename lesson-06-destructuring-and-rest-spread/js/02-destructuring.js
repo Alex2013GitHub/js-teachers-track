0d9387c64250b8d7f2bd490faeb8a032ef392122
Alex2013GitHub <96023225+Alex2013GitHub@users.noreply.github.com>
2022-07-03 21:36:12 +0200

    destructuring

--- lesson-06-destructuring-and-rest-spread/js/02-destructuring.js
+++ lesson-06-destructuring-and-rest-spread/js/02-destructuring.js
@@ -82,17 +82,17 @@ const [red, green, blue] = rgb;
 /*
  * Операция rest (сбор)
  */
-const profile = {
-  name: 'Jacques Gluke',
-  tag: 'jgluke',
-  location: 'Ocho Rios, Jamaica',
-  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
+// const profile = {
+//   name: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   location: 'Ocho Rios, Jamaica',
+//   avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
 
 // const { name, tag, location, ...restProps } = profile;
 
@@ -113,16 +113,16 @@ const showProfileInfo = function (userProfile) {
   console.log(restProps);
 };
 
-// const profile = {
-//   name: 'Jacques Gluke',
-//   tag: 'jgluke',
-//   location: 'Ocho Rios, Jamaica',
-//   avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
-//   stats: {
-//     followers: 5603,
-//     views: 4827,
-//     likes: 1308,
-//   },
-// };
+const profile = {
+  name: 'Jacques Gluke',
+  tag: 'jgluke',
+  location: 'Ocho Rios, Jamaica',
+  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
 
-// showProfileInfo(profile);
+showProfileInfo(profile);
