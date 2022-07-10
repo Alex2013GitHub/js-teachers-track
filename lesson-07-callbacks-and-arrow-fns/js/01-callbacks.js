4cb8ba4dbbfef34a4fe302bdb3230c93f1a65bc9
Alex2013GitHub <96023225+Alex2013GitHub@users.noreply.github.com>
2022-07-10 17:02:11 +0200

    callback

--- lesson-07-callbacks-and-arrow-fns/js/01-callbacks.js
+++ lesson-07-callbacks-and-arrow-fns/js/01-callbacks.js
@@ -20,7 +20,6 @@
 
 // fnA('qweqwe', fnB)
 
-
 /*
  * функция doMath(a, b, callback)
  */
@@ -40,8 +39,6 @@
 
 // doMath(2, 3, add);
 // doMath(10, 3, sub);
-
-
 
 // const doMath = function (a, b, callback) {
 //   const result = callback(a, b);
@@ -63,38 +60,37 @@
 
 const buttonRef = document.querySelector(".js-button");
 
-const handleBtnClick = function(){
-  console.log('Клик по кнопке '+ Date.now())
-}
+const handleBtnClick = function () {
+  console.log("Клик по кнопке " + Date.now());
+};
 
-buttonRef.addEventListener('click', handleBtnClick)
+buttonRef.addEventListener("click", handleBtnClick);
 
 /*
  * Отложенный вызов: геолокация
  */
 
-const onRequestSuccess = function(position){
-  console.log("Это вызов onGetPositionSuccess");
-  console.log(position);
-};
+// const onRequestSuccess = function(position){
+//   console.log("Это вызов onGetPositionSuccess");
+//   console.log(position);
+// };
 
-const onGetPositionError = function (error){
-  console.log(error);
-};
+// const onGetPositionError = function (error){
+//   console.log(error);
+// };
 
-window.navigator.geolocation.getCurrentPosition(
-  onRequestSuccess,
-  onGetPositionError,
-);
-
-
+// window.navigator.geolocation.getCurrentPosition(
+//   onRequestSuccess,
+//   onGetPositionError,
+// );
 
 /*
  * Отложенный вызов: интервалы
  */
 
-// const callback = function () {
+// const callback = function(){
 //   console.log("Через 2 секунды внутри колбека в таймауте");
+
 // };
 
 // console.log('В коде перед таймаутом');
@@ -106,62 +102,62 @@ window.navigator.geolocation.getCurrentPosition(
  * - API URL: https://pokeapi.co/api/v2/pokemon
  */
 
-// const onRequestSuccess = function (response) {
+// const onRequestSuccess = function (response){
 //   console.log("Вызов функции onRequestSuccess после успешного ответа бекенда");
 //   console.log(response);
-// };
+// }
 
 // fetch('https://pokeapi.co/api/v2/pokemon')
-//     .then(res => res.json())
-//     .then(onRequestSuccess);
+// .then(res => res.json())
+// .then(onRequestSuccess);
 
 /*
  * Фильтр
  */
 
-// const filter = function (array, test) {
-//   const filteredArray = [];
+const filter = function (array, test) {
+  const filteredArray = [];
 
-//   for (const el of array) {
-//     console.log(el);
-//     const passed = test(el);
+  for (const el of array) {
+    console.log(el);
+    const passed = test(el);
 
-//     if (passed) {
-//       filteredArray.push(el);
-//     }
-//   }
-
-//   return filteredArray;
-// };
+    if(passed){
+      filteredArray.push(el);
+    }
+    
+  }
+  return filteredArray;
+};
 
 // 1. надо передать функцию
 // 2. функция получает элемент массива
 // 3. если элемент массива удовлетворяет условию то функция вернет true
 // 3. если элемент массива НЕ удовлетворяет условию то функция вернет false
 
-// const callback1 = function (value) {
-//   return value >= 3;
-// };
+const callback1 = function (value) {
+  return value >= 3;
+};
 
-// const r1 = filter([1, 2, 3, 4, 5], callback1);
-// console.log(r1);
+const r1 = filter([1, 2, 3, 4, 5], callback1);
+console.log(r1);
 
-// const callback2 = function (value) {
-//   return value <= 4;
-// };
+const callback2 = function(value){
+  return value <= 4;
+};
 
-// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8], callback2);
-// console.log(r2);
+const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8], callback2);
+console.log(r2);
 
-// const fruits = [
-//   { name: "apples", quantity: 200, isFresh: true },
-//   { name: "grapes", quantity: 150, isFresh: false },
-//   { name: "bananas", quantity: 100, isFresh: true },
-// ];
+const fruits = [
+  { name: "apples", quantity: 200, isFresh: true },
+  { name: "grapes", quantity: 150, isFresh: false },
+  { name: "bananas", quantity: 100, isFresh: true },
+];
 
-// const getFruitsWithQuantity = function (fruit) {
-//   return fruit.quantity >= 120;
-// };
+const getFruitsWithQuantity = function (fruit){
+  return fruit.quantity >= 120;
+}
 
-// const r3 = filter(fruits, getFruitsWithQuantity);
-// console.log(r3);
+const r3 = filter(fruits, getFruitsWithQuantity);
+console.log(r3);
