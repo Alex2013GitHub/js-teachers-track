92e60661dd318d1e4294c18dd4563a5454e1f5c0
Alex2013GitHub <96023225+Alex2013GitHub@users.noreply.github.com>
2022-07-10 13:10:17 +0200

    lesson_objects

--- lesson-05-objects/js/03-cart.js
+++ lesson-05-objects/js/03-cart.js
@@ -93,3 +93,56 @@ console.table(cart.getItems());
 // cart.decreaseQuantity('🍋');
 // cart.decreaseQuantity('🍋');
 console.table(cart.getItems());
+
+
+
+
+
+// -------
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Dragon breath", price: 780 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+//   // Change code below this line
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//     for (const potion of this.potions){
+//       if (potion.name === newPotion.name) {
+//       return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//     }
+//     }
+    
+//     this.potions.push(newPotion);
+//   },
+//   removePotion(potionName) {
+//     const {potions} = this;
+// for (let i=0; i < potions.length; i += 1){
+//   const {name} = potions[i];
+// if (potionName === name) {
+//   potions.splice(i, 1);
+      
+//     }
+
+// }
+    
+// return `Potion ${potionName} is not in inventory!`;
+    
+// },
+//   updatePotionName(oldName, newName) {
+//      for (const potion of this.potions){
+//        if (oldName === potion.name ){
+//          potion.name = newName;
+//        }
+//      }
+
+    
+//       return `Potion ${oldName} is not in inventory!`;
+//   }
+
+    
+//   // Change code above this line
+// }
