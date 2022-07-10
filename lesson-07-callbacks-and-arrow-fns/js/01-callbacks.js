0d7903c6d03fa50e7c96ef0e16d459f0a8bce3ed
Alex2013GitHub <96023225+Alex2013GitHub@users.noreply.github.com>
2022-07-10 14:26:02 +0200

    callback

--- lesson-07-callbacks-and-arrow-fns/js/01-callbacks.js
+++ lesson-07-callbacks-and-arrow-fns/js/01-callbacks.js
@@ -7,28 +7,47 @@
  *   или возвращает функцию как результат своей работы называется «функцией высшего порядка»
  */
 
-// const fnA = function (message, callback) {
-//     console.log(message);
+// const fnA = function(message, callback){
+//   console.log(message);
 
-//     console.log(callback);
-//     callback(100);
+//   console.log(callback);
+//   callback(100);
 // };
 
-// const fnB = function (number) {
-//     console.log('Это лог при вызове fnB', number);
+// const fnB = function(number){
+//   console.log('Это лог при вызове fnB', number);
 // };
 
-// fnA('qweqwe', fnB);
+// fnA('qweqwe', fnB)
+
 
 /*
  * функция doMath(a, b, callback)
  */
+// const doMath = function (a, b, callback){
+//   const result = callback(a, b);
 
-const doMath = function (a, b, callback) {
-  const result = callback(a, b);
+//   console.log(result);
+// };
 
-  console.log(result);
-};
+// const add = function(x, y){
+//   return x+y;
+// };
+
+// const sub = function(x, y){
+//   return x-y;
+// };
+
+// doMath(2, 3, add);
+// doMath(10, 3, sub);
+
+
+
+// const doMath = function (a, b, callback) {
+//   const result = callback(a, b);
+
+//   console.log(result);
+// };
 
 // doMath(2, 3, function (x, y) {
 //     return x + y;
@@ -44,38 +63,39 @@ const doMath = function (a, b, callback) {
 
 const buttonRef = document.querySelector(".js-button");
 
-const handleBtnClick = function () {
-  console.log("Клик по кнопке " + Date.now());
-};
+const handleBtnClick = function(){
+  console.log('Клик по кнопке '+ Date.now())
+}
 
-// buttonRef.addEventListener('click', handleBtnClick);
+buttonRef.addEventListener('click', handleBtnClick)
 
 /*
  * Отложенный вызов: геолокация
  */
 
-const onGetPositionSuccess = function (position) {
+const onRequestSuccess = function(position){
   console.log("Это вызов onGetPositionSuccess");
   console.log(position);
 };
 
-const onGetPositionError = function (error) {
-  console.log("Это вызов onGetPositionError");
+const onGetPositionError = function (error){
   console.log(error);
 };
 
-// window.navigator.geolocation.getCurrentPosition(
-//     onGetPositionSuccess,
-//     onGetPositionError,
-// );
+window.navigator.geolocation.getCurrentPosition(
+  onRequestSuccess,
+  onGetPositionError,
+);
+
+
 
 /*
  * Отложенный вызов: интервалы
  */
 
-const callback = function () {
-  console.log("Через 2 секунды внутри колбека в таймауте");
-};
+// const callback = function () {
+//   console.log("Через 2 секунды внутри колбека в таймауте");
+// };
 
 // console.log('В коде перед таймаутом');
 // setTimeout(callback, 2000);
@@ -86,10 +106,10 @@ const callback = function () {
  * - API URL: https://pokeapi.co/api/v2/pokemon
  */
 
-const onRequestSuccess = function (response) {
-  console.log("Вызов функции onRequestSuccess после успешного ответа бекенда");
-  console.log(response);
-};
+// const onRequestSuccess = function (response) {
+//   console.log("Вызов функции onRequestSuccess после успешного ответа бекенда");
+//   console.log(response);
+// };
 
 // fetch('https://pokeapi.co/api/v2/pokemon')
 //     .then(res => res.json())
@@ -99,49 +119,49 @@ const onRequestSuccess = function (response) {
  * Фильтр
  */
 
-const filter = function (array, test) {
-  const filteredArray = [];
+// const filter = function (array, test) {
+//   const filteredArray = [];
 
-  for (const el of array) {
-    console.log(el);
-    const passed = test(el);
+//   for (const el of array) {
+//     console.log(el);
+//     const passed = test(el);
 
-    if (passed) {
-      filteredArray.push(el);
-    }
-  }
+//     if (passed) {
+//       filteredArray.push(el);
+//     }
+//   }
 
-  return filteredArray;
-};
+//   return filteredArray;
+// };
 
 // 1. надо передать функцию
 // 2. функция получает элемент массива
 // 3. если элемент массива удовлетворяет условию то функция вернет true
 // 3. если элемент массива НЕ удовлетворяет условию то функция вернет false
 
-const callback1 = function (value) {
-  return value >= 3;
-};
+// const callback1 = function (value) {
+//   return value >= 3;
+// };
 
-const r1 = filter([1, 2, 3, 4, 5], callback1);
-console.log(r1);
+// const r1 = filter([1, 2, 3, 4, 5], callback1);
+// console.log(r1);
 
-const callback2 = function (value) {
-  return value <= 4;
-};
+// const callback2 = function (value) {
+//   return value <= 4;
+// };
 
-const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8], callback2);
-console.log(r2);
+// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8], callback2);
+// console.log(r2);
 
-const fruits = [
-  { name: "apples", quantity: 200, isFresh: true },
-  { name: "grapes", quantity: 150, isFresh: false },
-  { name: "bananas", quantity: 100, isFresh: true },
-];
+// const fruits = [
+//   { name: "apples", quantity: 200, isFresh: true },
+//   { name: "grapes", quantity: 150, isFresh: false },
+//   { name: "bananas", quantity: 100, isFresh: true },
+// ];
 
-const getFruitsWithQuantity = function (fruit) {
-  return fruit.quantity >= 120;
-};
+// const getFruitsWithQuantity = function (fruit) {
+//   return fruit.quantity >= 120;
+// };
 
-const r3 = filter(fruits, getFruitsWithQuantity);
-console.log(r3);
+// const r3 = filter(fruits, getFruitsWithQuantity);
+// console.log(r3);
