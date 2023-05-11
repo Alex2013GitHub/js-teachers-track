1ed1e63af707bb940c8f8a4f2379a2b7f3eccee0
Oleksandr Lapin <96023225+Alex2013GitHub@users.noreply.github.com>
2023-05-11 21:43:56 +0200

    Sacha

--- lesson-12-events/js/01-event-listeners.js
+++ lesson-12-events/js/01-event-listeners.js
@@ -37,5 +37,19 @@ function logMessage(){
   console.log('Клик по целевой кнопке')
 }
 
+// ===============================
 
 
+// const targetBtn = document.querySelector('.js-target-btn');
+// const addListenerBtn = document.querySelector('.js-add-listener');
+// const removeListenerBtn = document.querySelector('.js-remove-listener');
+
+// targetBtn.addEventListener('click', () => {
+//   console.log('Клик');
+// })
+
+
+
+// function logMessage() {
+//   console.log('Клик по целевой кнопке');
+// }
