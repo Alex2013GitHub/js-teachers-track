7750428d83e2a50bf52938bfcc986755069dc5d9
Oleksandr Lapin <96023225+Alex2013GitHub@users.noreply.github.com>
2023-08-02 22:42:53 +0200

    o_repeta

--- lesson-16-webstorage/src/js/01-localstorage.js
+++ lesson-16-webstorage/src/js/01-localstorage.js
@@ -1,5 +1,15 @@
 console.log(localStorage);
 
+// localStorage.setItem('my-data', JSON.stringify({ name: 'Mango', age: 2 }));
+
+// const savedData = localStorage.getItem('my-data');
+// console.log('savedData', savedData);
+
+// const parsedData = JSON.parse(savedData);
+// console.log('parsedData', parsedData);
+
+// ===============================
+
 localStorage.setItem('my-data', JSON.stringify({ name: 'Mango', age: 2 }));
 
 const savedData = localStorage.getItem('my-data');
